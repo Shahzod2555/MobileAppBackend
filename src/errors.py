7af74e42b19fb6abcabcc7a1c0f1d28e57fbd67a
Shahzod <shahzhodergashev2555@icloud.com>
2024-12-17 20:10:52 +0300

    update requirements.txt file

--- src/errors.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException
-
-class UserAlreadyExistsError(HTTPException):
-    def __init__(self):
-        super().__init__(status_code=400, detail="Пользователь с таким email или номером телефона уже существует.")
-
-class InvalidCredentialsError(HTTPException):
-    def __init__(self):
-        super().__init__(status_code=401, detail="Неверный номер телефона, email или пароль.")
